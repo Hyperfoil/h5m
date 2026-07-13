fa44f803f4d1a949ddd04a9aa281ddd089dc0a8d
Will Reichert <willr3@gmail.com>
2026-07-13 12:40:45 -0400

    fix RootNode to EphemeralMode.KEEP

--- src/main/java/io/hyperfoil/tools/h5m/entity/node/RootNode.java
+++ src/main/java/io/hyperfoil/tools/h5m/entity/node/RootNode.java
@@ -1,5 +1,6 @@
 package io.hyperfoil.tools.h5m.entity.node;
 
+import io.hyperfoil.tools.h5m.api.EphemeralMode;
 import io.hyperfoil.tools.h5m.api.NodeType;
 import io.hyperfoil.tools.h5m.entity.NodeEntity;
 import jakarta.persistence.DiscriminatorValue;
@@ -11,11 +12,18 @@ public class RootNode extends NodeEntity {
 
     public RootNode() {
         super();
+        super.ephemeral = EphemeralMode.KEEP;
+
     }
 
     @Override
     public NodeType type() {
         return NodeType.ROOT;
+    }
+
+
+    public void setEphemeral(EphemeralMode ephemeral){
+
     }
 
     //The root node does not shallow copy
